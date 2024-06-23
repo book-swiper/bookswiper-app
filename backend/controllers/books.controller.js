31b74259d85e37daa9fb4fcaa98c88c4b882d229
christian kocevar <christian.kocevar@qualitestgroup.com>
2024-06-23 23:05:56 +0200

    implement books controller and bookItem

--- backend/controllers/books.controller.js
+++ backend/controllers/books.controller.js
@@ -1,3 +1,49 @@
 import Book from "../models/bookModel.js";
 import { errorHandler } from "../utils/error.js";
 
+export const getBooks = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let title = req.query.title;
+
+    if (title === undefined || title === "false") {
+      title = { $in: [false, true] };
+    }
+
+    let author = req.query.author;
+
+    if (author === undefined || author === "false") {
+      author = { $in: [false, true] };
+    }
+
+    let publishYear = req.query.publishYear;
+
+    if (publishYear === undefined || publishYear === "false") {
+      publishYear = { $in: [false, true] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const books = await Book.find({
+      name: { $regex: searchTerm, $options: "i" },
+      Ftitle,
+      author,
+      publishYear,
+      // publishYear,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(books);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ frontend/src/pages/SearchBook.jsx
@@ -0,0 +1,271 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import BookItem from '../components/home/BookItem';
+
+export default function Search() {
+  const navigate = useNavigate();
+  const [sidebardata, setSidebardata] = useState({
+    searchTerm: '',
+    title: 'all',
+    author: false,
+    publishYear: false,
+    //offer: false,
+    sort: 'created_at',
+    order: 'desc',
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+
+
+  console.log(sidebardata);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    const typeFromUrl = urlParams.get('type');
+    const titleFromUrl = urlParams.get('title');
+    const authorFromUrl = urlParams.get('author');
+    const publishYearFromUrl = urlParams.get('publishYear');
+    const sortFromUrl = urlParams.get('sort');
+    const orderFromUrl = urlParams.get('order');
+
+    if (
+      searchTermFromUrl ||
+      titleFromUrl ||
+      authorFromUrl ||
+      publishYearFromUrl ||
+      //offerFromUrl ||
+      sortFromUrl ||
+      orderFromUrl
+    ) {
+      setSidebardata({
+        searchTerm: searchTermFromUrl || '',
+        title: titleFromUrl || 'all',
+        author: authorFromUrl === 'true' ? true : false,
+        publishYear: publishYearFromUrl === 'true' ? true : false,
+       // offer: offerFromUrl === 'true' ? true : false,
+        sort: sortFromUrl || 'created_at',
+        order: orderFromUrl || 'desc',
+      });
+    }
+
+    const fetchBooks = async () => {
+      setLoading(true);
+      setShowMore(false);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/book/get?${searchQuery}`);
+      const data = await res.json();
+      if (data.length > 8) {
+        setShowMore(true);
+      } else {
+        setShowMore(false);
+      }
+      setBooks(data);
+      setLoading(false);
+    };
+
+    fetchBooks();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    if (
+      e.target.id === 'all' ||
+      e.target.id === 'rent' ||
+      e.target.id === 'sale'
+    ) {
+      setSidebardata({ ...sidebardata, type: e.target.id });
+    }
+
+    if (e.target.id === 'searchTerm') {
+      setSidebardata({ ...sidebardata, searchTerm: e.target.value });
+    }
+
+    if (
+      e.target.id === 'title' ||
+      e.target.id === 'author' ||
+      e.target.id === 'publishYear'
+    ) {
+      setSidebardata({
+        ...sidebardata,
+        [e.target.id]:
+          e.target.checked || e.target.checked === 'true' ? true : false,
+      });
+    }
+
+    if (e.target.id === 'sort_order') {
+      const sort = e.target.value.split('_')[0] || 'created_at';
+
+      const order = e.target.value.split('_')[1] || 'desc';
+
+      setSidebardata({ ...sidebardata, sort, order });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set('searchTerm', sidebardata.searchTerm);
+    urlParams.set('title', sidebardata.title);
+    urlParams.set('author', sidebardata.author);
+    urlParams.set('publishYear', sidebardata.publishYear);
+    //urlParams.set('offer', sidebardata.offer);
+    urlParams.set('sort', sidebardata.sort);
+    urlParams.set('order', sidebardata.order);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  const onShowMoreClick = async () => {
+    const numberOfBooks = books.length;
+    const startIndex = numberOfBooks;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/book/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setBooks([...Books, ...data]);
+  };
+  return (
+    <div className='flex flex-col md:flex-row'>
+      <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-8'>
+          <div className='flex items-center gap-2'>
+            <label className='whitespace-nowrap font-semibold'>
+              Search Term:
+            </label>
+            <input
+              type='text'
+              id='searchTerm'
+              placeholder='Search...'
+              className='border rounded-lg p-3 w-full'
+              value={sidebardata.searchTerm}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='flex gap-2 flex-wrap items-center'>
+            <label className='font-semibold'>Type:</label>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='all'
+                className='w-5'
+                onChange={handleChange}
+                checked={sidebardata.type === 'all'}
+              />
+              <span></span>
+            </div>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='rent'
+                className='w-5'
+                onChange={handleChange}
+                checked={sidebardata.type === 'rent'}
+              />
+              <span></span>
+            </div>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='sale'
+                className='w-5'
+                onChange={handleChange}
+                checked={sidebardata.type === 'sale'}
+              />
+              <span></span>
+            </div>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='offer'
+                className='w-5'
+                onChange={handleChange}
+                checked={sidebardata.offer}
+              />
+              <span></span>
+            </div>
+          </div>
+          <div className='flex gap-2 flex-wrap items-center'>
+            <label className='font-semibold'></label>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='parking'
+                className='w-5'
+                onChange={handleChange}
+                checked={sidebardata.parking}
+              />
+              <span></span>
+            </div>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='author'
+                className='w-5'
+                onChange={handleChange}
+                checked={sidebardata.author}
+              />
+              <span>author</span>
+            </div>
+          </div>
+          <div className='flex items-center gap-2'>
+            <label className='font-semibold'>Sort:</label>
+            <select
+              onChange={handleChange}
+              defaultValue={'created_at_desc'}
+              id='sort_order'
+              className='border rounded-lg p-3'
+            >
+              <option value='regularPrice_desc'>Price high to low</option>
+              <option value='regularPrice_asc'>Price low to hight</option>
+              <option value='createdAt_desc'>Latest</option>
+              <option value='createdAt_asc'>Oldest</option>
+            </select>
+          </div>
+          <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+            Search
+          </button>
+        </form>
+      </div>
+      <div className='flex-1'>
+        <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
+          Book results:
+        </h1>
+
+
+        <div className='p-7 flex flex-wrap gap-4'>
+          {!loading && Books.length === 0 && (
+            <p className='text-xl text-slate-700'>No Book found!</p>
+          )}
+          {loading && (
+            <p className='text-xl text-slate-700 text-center w-full'>
+              Loading...
+            </p>
+          )}
+
+          {!loading &&
+            Books &&
+            Books.map((Book) => (
+              <BookItem key={Book._id} Book={Book} />
+            ))}
+
+          {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+          )}
+        </div>
+
+
+      </div>
+    </div>
+  );
+}
