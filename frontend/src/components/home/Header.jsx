a97152ab98d5190e3b678f5d8f3cc5c30b03b4fe
christian kocevar <christian.kocevar@qualitestgroup.com>
2024-06-23 21:21:17 +0200

    implement search bar

--- frontend/src/components/home/Header.jsx
+++ frontend/src/components/home/Header.jsx
@@ -9,8 +9,37 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { FaSearch } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+//const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+
+  };
+
+  useEffect(() => {
+
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+
+
 	return (
 		<AppBar position="static">
 			<Toolbar>
@@ -35,8 +64,22 @@ export default function Header() {
 					component="div"
 					sx={{ flexGrow: 1 }}
 				>
-					GeeksforGeeks Header
+					Book swiper app
 				</Typography>
+				        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
+                          <input
+                            type="text"
+                            placeholder="Search..."
+                            className="bg-transparent focus:outline-none w-24 sm:w-64"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+
+                          />
+                          <button>
+                          <FaSearch className="text-slate-600" />
+
+                          </button>
+                        </form>
 				<Button color="inherit">Login</Button>
 			</Toolbar>
 		</AppBar>
